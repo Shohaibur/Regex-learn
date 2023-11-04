5d65e115275d3e6909308769aa1e58711d228286
Shoaib <shoaibrochi9@gmail.com>
2023-11-05 03:18:22 +0600

    update literal and meta character file

--- literal-meta-character.js
+++ literal-meta-character.js
@@ -2,21 +2,48 @@ let re;
 let str;
 
 re = /nala/;
-str = "Nala";
+//case insensitive /foo/i;
+re = /nala/i;
+str = "My cat' name is Nala";
 
-//console.log(re.exec(str));
-//document.write(re.exec(str));
+//meta-characters
+// /^foo/ ; must start with foo;
+re=/^nala/i;
+str = "Nala is my cat's name";
+
+// /bar$/ ; must end with bar;
+re=/cat$/i;
+str = "Nala is the name of my Cat";
+
+// /./ ; matches any one character;
+re= /^n.la$/i;
+str = "Nula";
+re = /n.la/i;
+str = "My cat's name is not Nula";
+
+// /*/ ; 0 or more times; case insensitive;
+re = /N*ala/;
+str = "naeala is not my cat's name";
+str = "Naala is not my cat's name";
+
+// /foo?bar/ ; o is optional;
+re = /na?la/i;
+str = "Nla";
+re = /na?e?la/i;
+str = "naela";
+
+// /\why?/ ; escaping ; ? is a part of the string not optional
+re = /why\?$/ ; 
+str = "okay, why?";
 
 
-//console.log(re.exec(str));
 
 function reTest(re,str){
   if (re.test(str)){
-   // console.log(`'${str}' matches regex '${re.source}' `)
     document.write(`'${str}' matches regex '${re.source}'`);
 
   }else{
-    //console.log(`'${str}' doesn't matches regex`)
+
     document.write(`'${str}' doesn't match '${re.source}'`);
 
   }

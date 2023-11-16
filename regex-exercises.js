4bcbe02fbed8b034d891c2beebc76d8d0165a0ec
Shoaib <shoaibrochi9@gmail.com>
2023-11-16 23:58:07 +0600

    modified regex exercises

--- regex-exercises.js
+++ regex-exercises.js
@@ -1,21 +1,27 @@
 let re ;
 let str ;
 
-//Postal Code (matches exactly 4 digits
+// 1. Postal Code (matches exactly 4 digits
 re =/^[0-9]{4}$/;
 str="1234";
 
+// 2. Bangladeshi Phone number (i.e. 01xxxxxxxxx , +8801xxxxxxxxx , 8801xxxxxxxxx)
+re = /^01[0-9]{9}$/;
+str = "01882576086"
 
+re = /^\+8801[0-9]{9}$/;
+str = "+8801882576086";
 
-function reTest(re,str){
-  if (re.test(str)){
-    document.write(`'${str}' matches regex '${re.source}'`);
+re = /^8801[0-9]{9}$/;
+str = "8801882576086";
 
-  }else{
+re = /^(\+88)?(88)?01[0-9]{9}$/; // Combined and Grouping
+str = "+8801882576086"
 
-    document.write(`'${str}' doesn't match '${re.source}'`);
+//Custom Email address (i.e. 20-42424-1@student.aiub.edu)
+re = /^[0-9]{2}-[0-9]{5}-[0-9]{1}(@student.aiub.edu)$/;
+str = "22-42424-3@student.aiub.edu"
 
-  }
+//
 
-} 
-reTest(re, str);
+document.write(re.test(str));
